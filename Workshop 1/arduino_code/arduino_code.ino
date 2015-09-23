8d7b3dbbe3e1bbb1612585f18b48d6b34b22762e
Alexis Matelin <a.matelin@gmail.com>
2015-09-22 21:30:05 -0400

    Added status led, power led and timer button to the code
    Added a function to check if the button is pressed but needs improvement
    : we need to check for a state change (HIGH -> LOW -> HIGH) and not just
    ?LOW because it increments the timer way to fast.

--- Workshop 1/arduino_code/arduino_code.ino
+++ Workshop 1/arduino_code/arduino_code.ino
@@ -7,24 +7,44 @@ int UP = 0;    // when the tea is lifted out of the cup
 int STIR_HIGH = 140; // high position for stirring
 int STIR_LOW = 90; // low position for stirring
 
-// Delay between each servo position adjustment during stirring. 
-// A lower value is associated with a quicker stirring.
+// Delay between each servo position adjustment during stirring
+// A lower value is associated with a quicker stirring
 int STIR_SMOOTH = 50;
+
+int STATUS_LED = 4; // Used to indicate that stirring is in progress
+int POWER_LED = 5; // Always on when the Arduino is powered
+
+int TIMER_BUTTON = 6; // Used to increment the stirring time
+int stir_time; 
  
 void setup() 
 { 
   myservo.attach(3);  // attaches the servo on pin 3 to the servo object 
+  pinMode(STATUS_LED, OUTPUT);
+  pinMode(POWER_LED, OUTPUT);
+  digitalWrite(POWER_LED, HIGH);
+  digitalWrite(STATUS_LED, LOW);
   Serial.begin(9600);
 } 
  
 void loop() 
 { 
-/*	if (Serial.available()) {
+	/*if (Serial.available()) {
 		int pos = Serial.read();
 		myservo.write(pos);
+	}*/
+
+	// digitalWrite(STATUS_LED, HIGH);
+	// stir();
+	// digitalWrite(STATUS_LED, LOW);
+	digitalWrite(STATUS_LED, HIGH);
+	if (isButtonPressed()) {
+		stir_time++;
+		Serial.println(stir_time);
+		delay(500);
 	}
-*/
-stir();
+	
+	
 } 
 
 void stir() {
@@ -44,4 +64,12 @@ int pos;
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(STIR_SMOOTH);                       // waits 15ms for the servo to reach the position 
   } 
+}
+
+bool isButtonPressed() {
+/*
+When the button is pressed, the TIMER_BUTTON pin is connected to ground and thus reads LOW.
+Alternatively, when the button is not pressed the pin is connected to 5v through the resistor and reads HIGH
+*/
+	return (digitalRead(TIMER_BUTTON) ? false : true);
 }
